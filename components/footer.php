89fc02cda5d80769266b14f152e0f09c7758c7b2
hariom <hariommahato20@gmail.com>
2022-08-12 17:08:06 +0545

    initial commit

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="./admin/admin_login.php"> <i class="fas fa-angle-right"></i> Admin login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="9808899681"><i class="fas fa-phone"></i> 98088899681</a>
+         <a href="98088899681"><i class="fas fa-phone"></i> 98088899681</a>
+         <a href="mailto:hariommahato20@gmail.com"><i class="fas fa-envelope"></i>hariommahato20@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Bhkatapur ,Nepal </a>
+      </div>
+
+      <div class="box">      
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a> 
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. hariom mahato</span> | all rights reserved!</div>
+
+</footer>
